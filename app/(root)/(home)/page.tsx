4dde887c36a6bf03e5ff28f3747d1b8b71418d96
Sultan Bibarys <sultanbibarys@icloud.com>
2024-08-15 16:47:17 +0500

    implement themn=meprovider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
-	return <div>Home Page</div>
+	return (
+		<div>
+			<UserButton afterSwitchSessionUrl='/' />
+		</div>
+	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { ThemeProvider } from '@/context/ThemeProvider'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Metadata } from 'next'
-import { Inter, Space_Grotesk } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import React from 'react'
 import './globals.css'
 
@@ -8,12 +9,6 @@ const inter = Inter({
 	subsets: ['latin'],
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 	variable: '--font-inter',
-})
-
-const spaceGrotesk = Space_Grotesk({
-	subsets: ['latin'],
-	weight: ['300', '400', '500', '600', '700'],
-	variable: '--font-spaceGrotesk',
 })
 
 export const metadata: Metadata = {
@@ -31,20 +26,19 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<ClerkProvider
-			appearance={{
-				elements: {
-					formButtonPrimary: 'primary-gradient',
-					footerActionLink: 'primary-text-gradient hover:text-primary-500',
-				},
-			}}
-		>
-			<html lang='en'>
-				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-					<h1 className='h1-bold'>This is a some text</h1>
-					{children}
-				</body>
-			</html>
-		</ClerkProvider>
+		<html lang='en'>
+			<body className={`${inter.variable}`}>
+				<ClerkProvider
+					appearance={{
+						elements: {
+							formButtonPrimary: 'primary-gradient',
+							footerActionLink: 'primary-text-gradient hover:text-primary-500',
+						},
+					}}
+				>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
 	)
 }
