6db9490a9e4235ec77cc1baaea48d0605d7abbfc
Sultan Bibarys <sultanbibarys@icloud.com>
2024-08-24 20:13:15 +0500

    add

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,14 @@
 import Question from '@/components/forms/Question'
 import { getUserById } from '@/lib/actions/user.actions'
+import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 
 export default async function AskQuestionPage() {
-	// const { userId } = auth()
-	const userId = 'CL123456789'
+	const { userId } = auth()
+
 	if (!userId) redirect('/sign-in')
 	const mongoUser = await getUserById({ userId })
-	console.log(mongoUser)
+
 	return (
 		<div>
 			<h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
