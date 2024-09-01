070e527ef4d5190284df78214ea9d162226dc535
Sultan Bibarys <sultanbibarys@icloud.com>
2024-09-01 20:01:19 +0500

    add

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -7,7 +7,7 @@ export default async function AskQuestionPage() {
   const userId = "clerk_12345";
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
-
+  console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
